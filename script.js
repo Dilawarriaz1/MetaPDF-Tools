e05cdd0c3abed0b42e479ba65b012e0699cbd2d2
Dilawarriaz1 <viralvideos1441@gmail.com>
2025-07-15 00:36:37 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function loadTool(toolName) {
-    document.getElementById("toolContainer").innerHTML = `<p>Loading ${toolName.replace(/([A-Z])/g, ' $1')}...</p>`;
-    // Dynamically load the tool (placeholder for now)
-}
-
-function showHelp() {
-    alert("Use the toolbar to select a tool.\nEach tool helps you convert or modify PDF documents.");
-}
